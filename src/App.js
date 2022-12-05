6b31ce9cead09d7110bac043c92c90ef81624885
SindidR <sindidreedh@gmail.com>
2022-12-05 18:39:26 -0500

    app-js-work-in-progress

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Section"
+import Section from "./components/Section";
+import NavBar from "./components/Navbar";
+
+function App() {
+    return (
+        <div>
+            <NavBar />
+            <Header />
+            <Section />
+        </div>
+    );
+}
+
+export default App;
